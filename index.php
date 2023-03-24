6f1b62bac36003dd147623ddd88f932c9dbe1283
Cyrille MdC <cyrille.mdc@gmail.com>
2023-03-24 10:15:54 +0100

    now generates static html

--- index.php
+++ index.php
@@ -4,6 +4,9 @@ $req = explode("/", ltrim($_SERVER['REQUEST_URI'], "/"));
 $url_base = '/' . $req[0] . '/' . $req[1] . '/';
 $page = $req[2];
 
+// start output buffering to save to static html
+ob_start();
+
 include 'html/header.html';
 include 'php/nav.php';
 
@@ -33,5 +36,17 @@ default:
 echo '</div>';
 
 include 'html/footer.html';
+
+
+// write output buffer
+$dir_path = 'export/' . $page;
+if (!is_dir($dir_path)) {
+    mkdir($dir_path, 0777);
+}
+
+$file_path = $dir_path . (strcmp($page, '') === 0 ? '' : '/') . 'index.html';
+if (!file_put_contents($file_path, ob_get_contents())) {
+    echo "ERROR SAVING BUFFER";
+}
 
 // EOF
