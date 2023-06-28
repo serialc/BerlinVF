2c4034255e281c9821804556f774918e23b60887
serialc <cyrille@gmail.com>
2023-06-28 18:25:29 +0200

    methods draft complete

--- www/php/methods.php
+++ www/php/methods.php
@@ -3,6 +3,26 @@
 // purpose: differentiates content
 
 switch ($subpage) {
+case 'storymaps':
+    include 'html/methods/storymaps.html';
+    break;
+
+case 'visualmedia':
+    include 'html/methods/visual_media.html';
+    break;
+
+case 'interviews':
+    include 'html/methods/interviews.html';
+    break;
+
+case 'reflexive':
+    include 'html/methods/reflexive.html';
+    break;
+
+case 'observations':
+    include 'html/methods/observations.html';
+    break;
+
 case 'archives':
     include 'html/methods/archive.html';
     break;
