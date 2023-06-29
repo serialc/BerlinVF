d50973987b8fffe28f52e3f8fb49d5954ce607ed
serialc <cyrille@gmail.com>
2023-06-29 13:03:52 +0200

    exifmapper added

--- www/php/stories.php
+++ www/php/stories.php
@@ -3,9 +3,14 @@
 // purpose: differentiates content
 
 switch ($subpage) {
+case 'imap':
+    include 'html/stories/imap.html';
+    break;
+
 case 'history':
     include 'html/stories/history.html';
     break;
+
 case 'building':
     include 'html/stories/building.html';
     break;
